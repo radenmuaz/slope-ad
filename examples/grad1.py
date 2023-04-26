fixing broadcast and reduce sum

examples/grad1.py
@@ -1,23 +1,25 @@
+from autodidax import linearize_flat
 import myad
 import numpy as np
-
+from myad import ops
 def f(x):
     y = x * x
     y = y + x
     return y
+
     
 # x, x_dot = np.array([3.0]), np.array([1.0])
 # y = f(x)
 # print('eval', y)
 
-def add_one_to_a_scalar(scalar):
-    assert np.ndim(scalar) == 0
-    return np.array(1) + scalar
+# def add_one_to_a_scalar(scalar):
+#     assert np.ndim(scalar) == 0
+#     return np.array(1) + scalar
 
-vector_in = np.arange(3.0)
-vector_out = myad.vmap(add_one_to_a_scalar, (0,))(vector_in)
-print(vector_in)
-print(vector_out)
+# vector_in = np.arange(3.0)
+# vector_out = myad.vmap(add_one_to_a_scalar, (0,))(vector_in)
+# print(vector_in)
+# print(vector_out)
 
 # x, x_dot = np.array([3.0]), np.array([1.0])
 # y, y_dot = myad.jvp(f, (x,), (x_dot,))
@@ -30,8 +32,25 @@ print(vector_out)
 # y_dot = f_lin(x_dot)
 # print(y_dot)
 
-# g = myad.grad(f)
-# l = g(1)
+
+
+def f(x):
+    y = ops.ReduceSum.do(x, axis=(0,))
+    return y
+
+g = myad.grad(f)
+l = g(np.ones((3)))
+print(l)
+
+
+# def f(x):
+#     y = ops.Broadcast.do(x, shape=(3,))
+#     # y = ops.ReduceSum.do(y, axis=(0,))
+#     return y
+
+# # g = myad.grad(f)
+# # l = g(np.ones((1)))
+# l = myad.jvp(f, (np.ones([1,]),), (np.ones([1,]),))
 # print(l)
 
 # print('jvp', y, y_dot)

examples/grad2.py
@@ -6,46 +6,19 @@ from myad import F
 # x = np.ones((1, 3))
 # y = np.ones((3, 1))
 
-x = np.array([[1, 2, 3]]).T
-y = np.array([[1, 2, 3]])
-out = F.dot(x, y)
-print('in')
-print(x, x.shape)
-print(y, y.shape)
-print('out')
-print(out)
-print(out.shape)
-    
-# x, x_dot = np.array([3.0]), np.array([1.0])
-# y = f(x)
-# print('eval', y)
+x = np.array([[1, 2, 3]])
+y = np.array([[1, 2, 3]]).T
+# x = np.array([[1, 2, 3], [1, 2, 3]]).T
+# y = np.array([[1, 2, 3], [1, 2, 3]])
 
-# def add_one_to_a_scalar(scalar):
-#     assert np.ndim(scalar) == 0
-#     return np.array(1) + scalar
+# out = F.dot(x, y)
 
-# vector_in = np.arange(3.0)
-# vector_out = myad.vmap(add_one_to_a_scalar, (0,))(vector_in)
-# print(vector_in)
-# print(vector_out)
+# print('in')
+# print(x, x.shape)
+# print(y, y.shape)
+# print('out')
+# print(out)
+# print(out.shape)
 
-# x, x_dot = np.array([3.0]), np.array([1.0])
-# y, y_dot = myad.jvp(f, (x,), (x_dot,))
-# print('jvp', y, y_dot)
-
-
-# x, x_dot = np.array([3.0]), np.array([1.0])
-# y, f_lin = myad.linearize(f, x)
-# print(y)
-# y_dot = f_lin(x_dot)
-# print(y_dot)
-
-# g = myad.grad(f)
-# l = g(1)
-# print(l)
-
-# print('jvp', y, y_dot)
-
-# jaxpr, consts, _ = myad.make_jaxpr(f, ArrayShape.from_numpy(x))
-# print('jaxpr')
-# print(jaxpr)
+f = myad.grad(F.dot)
+l = f(x,y)

myad/F.py
@@ -1,65 +1,40 @@
 import myad
 from myad import ops
+import numpy as np
 
-# UnaryOps
-def identity(x):
-    return myad.RT.bind1(ops.Identity, x)
-def exp(x):
-    return myad.RT.bind1(ops.Exp, x)
-def log(x):
-    return myad.RT.bind1(ops.Log, x)
-def neg(x):
-    return myad.RT.bind1(ops.Neg, x)
-
-# BinaryOps
-def add(x, y):
-    return myad.RT.bind1(ops.Add, x, y)
-def sub(x, y):
-    return myad.RT.bind1(ops.Sub, x, y)
-def mul(x, y):
-    return myad.RT.bind1(ops.Mul, x, y)
-def div(x, y):
-    return myad.RT.bind1(ops.Div, x, y)
-def pow(x, y):
-    return myad.RT.bind1(ops.Pow, x, y)
-
-# ReduceOps
-def reduce_sum(x, axis):
-    return myad.RT.bind1(ops.ReduceSum, x, axis)
-def reduce_max(x, axis):
-    return myad.RT.bind1(ops.ReduceMax, x, axis)
-
-# ShapeOps
-
-def broadcast(x, shape):
-    return myad.RT.bind1(ops.Broadcast, x, shape)
-
-def reshape(x, shape):
-    return myad.RT.bind1(ops.Reshape, x, shape)
-
-def transpose(x, perm):
-    return myad.RT.bind1(ops.Transpose, x, perm)
-
-# ML
 def expand_dims(x, axis):
     reshape_shape = list(x.shape)
     if type(axis) is int:
         axis = (axis,)
     for a in axis:
         reshape_shape.insert(a, 1)
-    x = reshape(x, reshape_shape)
+    x = ops.Reshape.do(x, reshape_shape)
     return x
 
 def dot(x, y):
-    a, b = x.shape[-2], x.shape[-1]
-    c, d = y.shape[-2], y.shape[-1]
-    assert b == c
-    y = transpose(y, (-1, -2))
+    # a, b = x.shape[-2], x.shape[-1]
+    # c, d = y.shape[-2], y.shape[-1]
+    # assert b == c
+    y = ops.Transpose.do(y, perm=(-1, -2))
     x = expand_dims(x, -3)
     y = expand_dims(y, -2)
     br_shape = (d, a, b)
-    x = broadcast(x, (*x.shape[:-3], *br_shape))
-    y = broadcast(x, (*y.shape[:-3], *br_shape))
+    x = ops.Broadcast.do(x, perm=(*x.shape[:-3], *br_shape))
+    y = ops.Broadcast.do(x, perm=(*y.shape[:-3], *br_shape))
     x = x * y
-    x = reduce_sum(x, -1)
+    x = ops.ReduceSum.do(x, -1)
     return x
+
+def relu(x):
+    return max(x, np.zeros(x.shape, x.dtype))
+
+def softmax(x, axis):
+    m = x - ops.ReduceMax.do(x, axis)
+    e = ops.Exp.do(m)
+    s_e = ops.ReduceSum.do(e, axis)
+    
+def cross_entropy(x, y):
+    return x * ops.Log.do(y)
+
+def mse(x, y):
+    return (x - y)**2

myad/core.py
@@ -275,11 +275,20 @@ class Tracer:
     def __nonzero__(self):
         return self.aval._nonzero(self)
 
-    @classmethod
-    def get_aval(cls, x):
-        if isinstance(x, cls):
+    # @classmethod
+    # def get_aval(cls, x):
+    #     if isinstance(x, cls):
+    #         return x.aval
+    #     elif type(x) in cls.TYPES:
+    #         return ValuedArrayShape(np.array(x))
+    #     else:
+    #         raise TypeError(x)
+    
+    @staticmethod
+    def get_aval(x):
+        if isinstance(x, Tracer):
             return x.aval
-        elif type(x) in cls.TYPES:
+        elif type(x) in Tracer.TYPES:
             return ValuedArrayShape(np.array(x))
         else:
             raise TypeError(x)
@@ -1080,38 +1089,14 @@ def eval_jaxpr_transposed(
     for eqn in jaxpr.eqns[::-1]:
         primals_in = list_map(read_primal, eqn.inputs)
         cts_in = list_map(read_cotangent, eqn.out_binders)
-        rule = eqn.op.T
-        cts_out = rule(cts_in, *primals_in, **eqn.params)
+        cts_out = eqn.op.T(cts_in, *primals_in, **eqn.params)
         list_map(write_cotangent, eqn.inputs, cts_out)
-    return [
+    ret = [
         read_cotangent(v)
         for v, x in zip(jaxpr.in_binders, args)
         if type(x) is UndefPrimal
     ]
-
-
-def mul_transpose_rule(cts, x, y):
-    (z_bar,) = cts
-    assert (type(x) is UndefPrimal) ^ (type(y) is UndefPrimal)
-    return [z_bar * y, None] if type(x) is UndefPrimal else [None, x * z_bar]
-
-
-def neg_transpose_rule(cts, x):
-    (ybar,) = cts
-    assert type(x) is UndefPrimal
-    return [-ybar]
-
-
-def add_transpose_rule(cts, x, y):
-    (z_bar,) = cts
-    return [z_bar, z_bar]
-
-
-def reduce_sum_transpose_rule(cts, x, *, axis):
-    (y_bar,) = cts
-    return [np.broadcast_to(y_bar, x.aval.shape, axis)]
-
-
+    return ret
 
 def grad(f):
     def gradfun(x, *xs):

myad/ops.py
@@ -6,8 +6,8 @@ from abc import ABC, abstractmethod
 
 class Op(ABC):
     @classmethod
-    def do(cls, *args):
-        return myad.RT.bind1(cls, *args)
+    def do(cls, *args, **params):
+        return myad.RT.bind1(cls, *args, **params)
 
     @classmethod
     @abstractmethod
@@ -184,7 +184,7 @@ class Sub(BinaryOp):
     @classmethod
     def jvp(cls, primals, tangents):
         (x, y), (x_dot, y_dot) = primals, tangents
-        return [x + y], [x_dot + y_dot]
+        return [x - y], [x_dot - y_dot]
 
     @classmethod
     def T(cls, cts, x, y):
@@ -235,19 +235,68 @@ class Pow(BinaryOp):
     def jvp(cls, primals, tangents):
         (x, y), (x_dot, y_dot) = primals, tangents
         return [x * y], [x_dot * y + x * y_dot]
+    
+    @classmethod
+    def T(cls, cts, x, y):
+        (z_bar,) = cts
+        return [z_bar / y, None]
 
 
 class Max(BinaryOp):
     @classmethod
     def eval(cls, x, y):
-        return [x**y]
+        return [np.greater(x, y)]
 
     @classmethod
     def jvp(cls, primals, tangents):
-        (x, y), (x_dot, y_dot) = primals, tangents
-        return [x * y], [x_dot * y + x * y_dot]
+        (x, y), _ = primals, tangents
+        out_primal = cls.do(x, y)
+        return [out_primal], [np.zeros(out_primal.shape, out_primal.dtype)]
+    
+    @classmethod
+    def T(cls, cts, x, y):
+        (z_bar,) = cts
+        return [z_bar, None]
 
 
+class Equal(BinaryOp):
+    @classmethod
+    def eval(cls, x, y):
+        return [np.equal(x, y)]
+
+    @classmethod
+    def jvp(cls, primals, tangents):
+        (x, y), _ = primals, tangents
+        out_primal = cls.do(x, y)
+        return [out_primal], [np.zeros(out_primal.shape, out_primal.dtype)]
+    
+    @classmethod
+    def T(cls, cts, x, y):
+        (z_bar,) = cts
+        return [z_bar, None]
+
+
+# max_p: core.Primitive = standard_naryop([_any, _any], 'max')
+# ad.defjvp2(max_p,
+#            lambda g, ans, x, y: mul(g, _balanced_eq(x, ans, y)),
+#            lambda g, ans, x, y: mul(g, _balanced_eq(y, ans, x)))
+# mlir.register_lowering(max_p, partial(_nary_lower_hlo, mlir.max_hlo))
+
+
+# def _balanced_eq(x, z, y):
+#   return div(select(_eq_meet(x, z), _ones(z), _zeros(z)),
+#              select(_eq_meet(y, z), _twos(z), _ones(z)))
+
+
+# def _eq_meet(a, b):
+#   a_dtype, b_dtype = _dtype(a), _dtype(b)
+#   if a_dtype != b_dtype:
+#     higher_dtype = dtypes.promote_types(a_dtype, b_dtype)
+#     if higher_dtype == a_dtype:
+#       a = convert_element_type(a, b_dtype)
+#     else:
+#       b = convert_element_type(b, a_dtype)
+#   return eq(a, b)
 # -----------------------
 # ReduceOps
 # -----------------------
@@ -259,30 +308,53 @@ class ReduceMax(ReduceOp):
         return [x.sum(axis)]
 
     @classmethod
-    def jvp(cls, primals, tangents):
-        (x, y), (x_dot, y_dot) = primals, tangents
-        return [x * y], [x_dot * y + x * y_dot]
+    def jvp(cls, primals, tangents, axis):
+        eval_out = cls.do(*primals)
+        eq_shape = [1 if i in axis else d 
+                    for i, d in enumerate(primals.shape)]
+        # we do equal with implicit broadcasting
+        locs = Equal.do(primals, Reshape.do(eval_out, eq_shape))
+        counts = ReduceSum.do(locs, axis)
+        jvp_out = ReduceSum.do(tangents * locs, axis) / counts
+        
+        return [eval_out], [jvp_out]
     
     @classmethod
     def T(cls, cts, x):
-        (y_bar,) = cts
+        (y_bar, axis) = cts
+        reshape_shape = list(y_bar.shape)
+        reshape_shape.insert(axis, 1)
+        y_bar = Reshape.do(x, reshape_shape)
         return [Broadcast(y_bar, x.aval.shape)]
-
 
 class ReduceSum(ReduceOp):
     @classmethod
-    def eval(cls, x, axis):
-        return [x.sum(axis)]
+    def eval(cls, x, *, axis):
+        return [np.sum(x, axis)]
 
     @classmethod
-    def jvp(cls, primals, tangents):
-        (x, y), (x_dot, y_dot) = primals, tangents
-        return [x * y], [x_dot * y + x * y_dot]
+    def jvp(cls, primals, tangents, *, axis):
+        (x,), (x_dot,) = primals, tangents
+        eval_out = cls.do(x, axis=axis)
+        jvp_out = cls.do(x_dot, axis=axis)
+        return [eval_out], [jvp_out]
 
     @classmethod
-    def T(cls, cts, x):
+    def T(cls, cts, x, *, axis):
         (y_bar,) = cts
-        return [Broadcast(y_bar, x.aval.shape)]
+        shape = [1 if i == axis[i] else k
+                for i, k in enumerate(x.aval.shape)]
+        y_bar = Reshape.do(y_bar, shape=shape)
+        return [Broadcast.do(y_bar, shape=x.aval.shape)]
+    
+
+# def _reduce_sum_transpose_rule(cotangent, operand, *, axes):
+#   assert ad.is_undefined_primal(operand)
+#   input_shape = operand.aval.shape
+#   broadcast_dimensions = tuple(np.delete(np.arange(len(input_shape)), axes))
+#   result = broadcast_in_dim(cotangent, input_shape, broadcast_dimensions)
+#   assert result.shape == input_shape
+#   return [result]
 
 
 # -----------------------
@@ -292,48 +364,66 @@ class ReduceSum(ReduceOp):
 
 class Broadcast(ShapeOp):
     @classmethod
-    def eval(cls, x, shape):
-        # for axis in sorted(axes):
-        #     x = np.expand_dims(x, axis)
+    def eval(cls, x, *, shape):
         return [np.broadcast_to(x, shape)]
 
     @classmethod
-    def jvp(cls, primals, tangents, shape, axes):
-        (x), (x_dot) = primals, tangents
-        return [cls.do(x, shape)], [cls.do(x_dot, shape)]
+    def jvp(cls, primals, tangents, *, shape):
+        (x,), (x_dot,) = primals, tangents
+        return [cls.do(x, shape=shape)], [cls.do(x_dot, shape=shape)]
 
     @classmethod
     def shape_eval(cls,
-        x: ArrayShape, shape: Sequence[int], axes: Sequence[int]
+        x: ArrayShape, shape: Sequence[int]
     ) -> List[ArrayShape]:
         return [ArrayShape(tuple(shape), x.dtype)]
-
-
-class Crop(ShapeOp):
+    
     @classmethod
-    def eval(cls, x, slice):
-        return [x[slice]]
+    def T(cls, cts, x, *, shape):
+        (y_bar,) = cts
+        axis = []
+        for idx, i, j in enumerate(zip(x.shape, shape)):
+            if i == 1 and i < j:
+                axis += [idx]
+        if axis is None:
+            raise ValueError
+        return [ReduceSum.do(y_bar, axis=axis)]
+
+
+# class Crop(ShapeOp):
+#     @classmethod
+#     def eval(cls, x, slice):
+#         return [x[slice]]
 
 
 class Reshape(ShapeOp):
     @classmethod
-    def eval(cls, x, perm):
-        return [np.reshape(x, perm)]
+    def eval(cls, x, *, shape):
+        return [np.reshape(x, shape)]
     
     @classmethod
-    def jvp(cls, primals, tangents, perm):
+    def jvp(cls, primals, tangents, *, shape):
         (x), (x_dot) = primals, tangents
-        return [cls.do(x, perm)], [cls.do(x_dot, perm)]
+        return [cls.do(x, shape)], [cls.do(x_dot, shape)]
+    
+
+    @classmethod
+    def T(cls, cts, x, *, shape):
+        (y_bar,) = cts
+        return [Reshape.do(y_bar, shape=x.aval.shape)]
 
 
 class Transpose(ShapeOp):
     @classmethod
-    def eval(cls, x, perm):
+    def eval(cls, x, *, perm):
         return [x.transpose(perm)]
-
-
-class Pad(ShapeOp):
+    
     @classmethod
-    def eval(cls, x, perm):
-        return [x.pad(perm)]
-
+    def jvp(cls, primals, tangents, *, perm):
+        (x), (x_dot) = primals, tangents
+        return [cls.do(x, perm)], [cls.do(x_dot, perm)]
+    
+    @classmethod
+    def T(cls, cts, x, *, perm):
+        (y_bar,) = cts
+        return [cls.do(y_bar, perm=perm)]
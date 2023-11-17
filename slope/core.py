a7c6a73d6dc7209eaf573a230cbfc67b67b73b6c
radenmuaz <raden.m.muaz@gmail.com>
2023-11-18 00:33:44 +0800

    inline

--- slope/core.py
+++ slope/core.py
@@ -626,9 +626,9 @@ class OperatorSet:
 
 class ProcedureSet:
     # def register(self, static_argnames=(), not_op=False):
-    def register(self, static_argnames=(), inline=True):
+    def register(self, static_argnames=(), not_op=True):
         def wrap(f):
-            f_procedure = self.new_procedure(f, static_argnames) if inline else f
+            f_procedure = self.new_procedure(f, static_argnames) if not not_op else f
             assert f.__name__ not in vars(self)
             setattr(self, f.__name__, f_procedure)
             return f_procedure

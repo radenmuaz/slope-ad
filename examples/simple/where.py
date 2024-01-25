53206df4ccb112d17c98227652ed1c399ae9a043
radenmuaz <raden.m.muaz@gmail.com>
2024-01-26 04:35:15 +0800

    where

--- examples/simple/where.py
+++ examples/simple/where.py
@@ -2,5 +2,7 @@ import slope
 
 x = slope.ones(5)
 w = slope.arange(5, dtype=x.dtype)
-y = (x > w).where(x,w)
-
+y = (x > w).where(x, w)
+print(f"{x=}")
+print(f"{w=}")
+print(f"(x > w).where(x,w) = {y}")

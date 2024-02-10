fixed batchnorm, should not use stop gradient

examples/nn/cifar10_convnet.py
@@ -54,25 +54,27 @@ def get_dataloader(images, labels, batch_size, shuffle=False):
 
 
 if __name__ == "__main__":
-    nepochs = 100
-    train_batch_size = 50  # TODO: must be multiple of dataset
-    test_batch_size = 50
+    nepochs = 160
+    train_batch_size = 100  # TODO: must be multiple of dataset
+    test_batch_size = 100
     train_images, train_labels, test_images, test_labels = get_cifar10()
 
     model = resnet(depth=8)
     # optimizer = nn.AdamW(model, lr=1e-1,weight_decay=1e-5)
-    optimizer = nn.SGD(model, lr=0.1)
-    # optimizer = nn.SGD(model, lr=0.1, momentum=0.9, weight_decay=1e-4)
+    optimizer = nn.SGD(model, lr=0.1, momentum=0.9, weight_decay=1e-4)
 
     train_dataloader, ntrain_batches = get_dataloader(train_images, train_labels, train_batch_size, shuffle=True)
     test_dataloader, ntest_batches = get_dataloader(test_images, test_labels, test_batch_size, shuffle=False)
+    # scheduler = nn.MultiStepLR(milestones=[80*ntrain_batches, 120*ntrain_batches], gamma=0.1)
 
     print("\nStarting training...")
     best_acc = 0.0
     for epoch in range(nepochs):
+        if epoch in [80, 120]:
+            optimizer.hp.lr = optimizer.hp.lr * 0.1
+            print(f"Optimzer lr now={optimizer.hp.lr.numpy()}")
         total_loss = 0.0
         true_positives = 0.0
-
         for i, batch in (pbar := tqdm(enumerate(train_dataloader()), total=ntrain_batches, leave=False)):
             loss, logits, model, optimizer, gmodel = train_step(model, batch, optimizer)
             # if i % 10 == 0: